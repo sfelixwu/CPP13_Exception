3009fce5fbb530902cf8c2d089bb4a41152d973e
sfelixwu <wu@cs.ucdavis.edu>
2023-11-27 14:59:24 -0800

    Add files via upload

--- ecs36b_Exception.h
+++ ecs36b_Exception.h
@@ -3,6 +3,8 @@
 #ifndef _ECS36B_EXCEPTION_
 #define _ECS36B_EXCEPTION_
 
+#include "ecs36b_Common.h"
+
 using namespace std;
 
 class ecs36b_special // not to be caught
@@ -33,6 +35,9 @@ class ecs36b_Exception : public std::exception, public ecs36b_special
 private:
 protected:
 public:
+  Json::Value exp_msg;
+  ecs36b_Exception(Json::Value m) : exp_msg {m} {};
+  ecs36b_Exception() {};
   virtual ~ecs36b_Exception() throw () {};
   virtual const char* what() const throw () { return "ecs36b"; };
 };
